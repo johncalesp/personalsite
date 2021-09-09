98efddc781bd6b062396d15a110bdbd4bd2968e7
johncalesp <81483067+johncalesp@users.noreply.github.com>
2021-09-09 12:34:26 -0400

    Update location info

--- src/component/home.js
+++ src/component/home.js
@@ -9,8 +9,7 @@ const Home = () => {
         <span className='span-bold'>
           my name is John Calderon. I am a telecommunication engineer{' '}
         </span>
-        currently based on Lima, Perú but in the next few months I will be
-        living in Toronto. As a telecommunication engineer, I have had the
+        currently based on Toronto, Canada. As a telecommunication engineer, I have had the
         opportunity to work in this field for many years. This has allowed me
         not only to acquire skills in different technologies, like routers,
         switches, servers and storage systems, but also to be promoted in my job
@@ -25,7 +24,7 @@ const Home = () => {
         <span className='span-bold'>
           As of now, I am looking for a part-time job in the field while I take
           my courses in the program Artificial Intelligence &amp; Machine
-          Learning from Lambton College starting in September 2021.
+          Learning from Lambton College.
         </span>{' '}
       </p>
     </div>
